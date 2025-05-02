620b61e7a31653fecc6f692d46122eef1d26b0e9
Elijah Aaron <elijahhager20@gmail.com>
2025-05-02 14:37:46 -0600

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -8,14 +8,20 @@ class SortVector{
         std::vector<int> sortVectorFunc(std::vector<int> vectorMain){
             std::vector<int> sortedVector = vectorMain;
             int cachedValue;
+            int j;
             
             for (int n = 0; n < sortedVector.size(); n++){
+                j = 0;
                 for (int i = 0; i < sortedVector.size() - 1; i++){
                     if (sortedVector[i] > sortedVector[i + 1]){
+                        j++;
                         cachedValue = sortedVector[i];
                         sortedVector[i] = sortedVector[i + 1];
                         sortedVector[i + 1] = cachedValue;
                     }
+                }
+                if (j == 0){
+                    break;
                 }
             }
         
@@ -29,12 +35,19 @@ int main(){
     std::vector<int> vectorMain(10000);
     SortVector SortVectorObj;
     
-    for (int i = 0; i < 10000; i++){
+    for (int i = 0; i < vectorMain.size(); i++){
         vectorMain[i] = (rand() % 100);
     }
     
+    std::cout << "Original vector:\n";
+    for (int i = 0; i < vectorMain.size(); i++){
+        std::cout << vectorMain[i] << ' ';
+    }
+    std::cout << "\n\n\n\n";
+    
     std::vector<int> sortedVector = SortVectorObj.sortVectorFunc(vectorMain);
     
+    std::cout << "Sorted vector:\n";
     for (int i = 0; i < sortedVector.size(); i++){
         std::cout << sortedVector[i] << ' ';
     }
